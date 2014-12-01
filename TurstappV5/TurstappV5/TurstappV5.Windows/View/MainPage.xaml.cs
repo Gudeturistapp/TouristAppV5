d3ba2edf0f8a0cdeef74f959f69d221adacbcc86
Pilsneren <parbst2600@gmail.com>
2014-12-01 11:38:38 +0100

    Default items created
    Usecase / sequence/ class/ design / views added

--- TurstappV5/TurstappV5/TurstappV5.Windows/View/MainPage.xaml.cs
+++ TurstappV5/TurstappV5/TurstappV5.Windows/View/MainPage.xaml.cs
@@ -1,21 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace TurstappV5
+namespace TurstappV5.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
