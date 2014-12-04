8fdaac9a344d63dfefbb754a48cd85391ef937b8
Danny <danny.nlsn0@gmail.com>
2014-12-04 13:29:33 +0100

    Added a little Login code

--- TuristAppV5/Model/Userdata.cs
+++ TuristAppV5/Model/Userdata.cs
@@ -8,5 +8,56 @@ namespace TuristAppV5.Model
 {
     class Userdata
     {
+
+        //The main user dictionary where username and login is stored. 
+        private static Dictionary<string, string> userDictionary = new Dictionary<string, string>(); 
+
+        public Dictionary<string, string> UserDictionary
+        {
+            get { return userDictionary; }
+            set { userDictionary = value; }
+        }
+
+
+
+        //Corner of the page shows login name after log in.
+        private static string loginName;
+
+        public string LoginName
+        {
+            get { return loginName; }
+            set { loginName = value; }
+        }
+
+        //If isLoggedIn=true, collapse vissiblity of loginButton and usernameButton.
+        private bool isLoggedIn;
+        
+
+        public void Login()
+        {
+            //if (UserDictionary.ContainsKey(Namebox.Text) && UserDictionary.ContainsValue(PassBox.Password)
+            //{
+                    //Success
+                    //isLoggedIn = true;
+            //}
+        }
+
+        public void CreateUser()
+        {
+            //if (!UserDictionary.ContainsKey(Namebox.Text)
+            //{
+                    //UserDictionary.Add(Namebox.Text, PassBox.Password);
+                    
+
+            //}
+            //else
+            //{
+                    //Error. User already exists. 
+            //}
+        }
+
+
+
+
     }
 }
